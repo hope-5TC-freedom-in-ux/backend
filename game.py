8d16eb05cc63a4a5b899aae170d4b4261cbfa1df
koalp <koalp@alpaga.dev>
2019-09-24 11:46:58 +0200

    refactor: change game to object
    Game was only defined in an imperative way. It was therefore difficult
    to add functionnalities without breaking, and to document.
    
    Games are now objects and functionnalities can easily be added.
    A PetiteBoiteNoire object have been created to contain the game

---
+++ game.py
@@ -0,0 +1,187 @@
+# TODO:
+# - refactoring : game update sessions themself
+# - refactoring : remove function that do only one thing
+# - refactoring : separate config from init
+# - ¿ refactoring : merge some objects with api ?
+# - use Score objets for each score
+
+from collections import OrderedDict
+from flask import session, redirect, Blueprint, current_app
+import toml
+
+game = Blueprint('game', __name__)
+
+
+@game.route('/')
+def main():
+    pbn = PetiteBoiteNoire()
+    return pbn.next()
+
+
+@game.route('/restart')
+def start():
+    pbn = PetiteBoiteNoire(restart=True)
+    return pbn.next()
+
+
+with open('conf/config.toml') as f:
+    config = toml.load(f)
+
+
+class PetiteBoiteNoire(OrderedDict):
+
+    def __init__(self,
+                 restart=False,
+                 max_time=100,
+                 max_privacy=100):
+
+        self.max_time = max_time
+        self.max_privacy = max_privacy
+
+        super().__init__(self._games(restart))
+        current_app.logger.debug(("Game initialised with game list {}"
+                                  .format(self)))
+
+    def _games(self, restart=False):
+        games = [c['name'] for c in config['games']]
+        if (session
+            and 'current' in session
+            and session['current'] == games[-1]):
+            restart = True
+
+        if not session or restart:
+            n = len(games)
+
+            game_dict = {g: Game(name=g,
+                                 max_time=self.max_time/n,
+                                 max_privacy=self.max_privacy/n)
+                         for g in games}
+            current_app.logger.debug(game_dict)
+            session['game_list'] = [game.dict() for game in game_dict.values()]
+
+        else:
+            game_dict = {game['name']: Game(**game)
+                         for game in session['game_list']}
+
+        return game_dict
+
+    def next(self):
+        games = list(self.keys())
+        if 'current' not in session or session['current'] == games[-1]:
+            session['current'] = games[0]
+        else:
+            current = session['current']
+            session['current'] = games[games.index(current) + 1]
+        return self.current().page
+
+    def current(self):
+        current = session['current']
+        return self[current]
+
+    def add_gain(self, privacy, time):
+        self.current().add_gain(privacy=privacy, time=time)
+        # TODO: find a more elegant solution
+        self._update_session()
+
+    def gain(self):
+        return self.current().gain()
+
+    def gains(self):
+        game_list = list(self.keys())
+        current_name = self.current().name
+        future_games = game_list[game_list.index(current_name) + 1:]
+
+        return [{'name': name, **game.gain()}
+                for name, game in self.items()
+                if name not in future_games]
+
+    def score(self):
+        return self.current().score()
+
+    def scores(self):
+        game_list = list(self.keys())
+        current_name = self.current().name
+        future_games = game_list[game_list.index(current_name) + 1:]
+
+        return [{'name': name, **game.score()}
+                for name, game in self.items()
+                if name not in future_games]
+
+    def name(self):
+        return self.current().name
+
+    def _update_session(self):
+        session['game_list'] = [game.dict() for game in self.values()]
+
+
+class Game():
+    name = None
+    privacy = None
+    time = None
+    page = None
+
+    def __init__(self,
+                 name,
+                 max_privacy=None,
+                 max_time=None,
+                 privacy=None,
+                 time=None):
+        self.name = name
+
+        self.max_privacy = max_privacy
+        self.privacy = privacy if privacy is not None else max_privacy
+
+        self.max_time = max_time
+        self.time = time if time is not None else max_time
+
+        self._get_config()
+
+    def _get_config(self):
+        games = {c['name']: c for c in config['games']}
+        game_config = games[self.name]
+
+        if 'content' in game_config:
+            page = game_config['content']
+        else:
+            page = redirect(game_config['url'])
+
+        self.page = page
+
+    def dict(self):
+        return {'name': self.name,
+                'privacy': self.privacy,
+                'max_privacy': self.max_privacy,
+                'time': self.time,
+                'max_time': self.max_time}
+
+    def __str__(self):
+        if self.max_privacy or self.max_time:
+            return ("name:{}, time:{}, privacy:{}, max_time:{}, max_privacy:{}"
+                    .format(self.name,
+                            self.time,
+                            self.privacy,
+                            self.max_time,
+                            self.max_privacy))
+        return ("name: {}, time:{}, privacy:{}"
+                .format(self.name, self.time, self.privacy))
+
+    def __repr__(self):
+        return "Game({})".format(str(self))
+
+    def add_gain(self, privacy=0, time=0):
+        self.privacy = max(self.privacy + privacy, 0)
+        if self.max_privacy:
+            self.privacy = min(self.max_privacy, self.privacy)
+
+        self.time = max(self.time + time, 0)
+        if self.max_time:
+            self.time = min(self.max_time, self.time)
+
+    def gain(self):
+        time = self.max_time - self.time if self.max_time else self.time
+        privacy = (self.max_privacy - self.privacy
+                   if self.max_privacy else self.privacy)
+        return {'time': time, 'privacy': privacy}
+
+    def score(self):
+        return {'time': self.time, 'privacy': self.privacy}
--- server.py
+++ server.py
@@ -5,59 +5,15 @@ import os
 
 import toml
 
-from page import Page
 from conf.secrets import session_key
 
 from api import blueprint
+from game import game
 
 app = Flask(__name__)
 app.register_blueprint(blueprint)
+app.register_blueprint(game, url_prefix='/')
 app.secret_key = session_key
 
-with open('conf/config.toml') as f:
-    config = toml.load(f)
-
-
-@app.route('/')
-def main():
-    if not session:
-        return redirect('/start')
-    return redirect('/game')
-
-
-@app.route('/game')
-def game():
-    game_list = session['game_list']
-
-    if not game_list:
-        return redirect('/end')
-
-    game_name = game_list.pop(0)
-    session['game_list'] = game_list
-    session['gains'].append({'name': game_name, 'privacy': 0, 'time': 0})
-
-    total_game_list = {c['name']: Page(c) for c in config['games']}
-    game = total_game_list[game_name]
-
-    return game.content
-
-
-@app.route('/start')
-def start():
-    session['game_list'] = [c['name'] for c in config['games']]
-    session['privacy'] = 0
-    session['time'] = 0
-    session['gains'] = []
-    page = Page(config['start'])
-
-    session['gains'].append({'name': 'start', 'privacy': 0, 'time': 0})
-
-    return page.content
-
-
-@app.route('/end')
-def end():
-    page = Page(config['end'])
-    session['gains'].append({'name': 'end', 'privacy': 0, 'time': 0})
-    return page.content
-
+if __name__ == "__main__":
+    app.run(debug=True)
